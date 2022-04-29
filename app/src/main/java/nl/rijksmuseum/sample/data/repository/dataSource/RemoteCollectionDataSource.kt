bf9576bebace1b76abb19ecd1def83b427eb6136
MalekMt-git <84619303+MalekMt-git@users.noreply.github.com>
2022-04-29 02:18:57 +0200

    Revert "Mock web server unit testing added"

--- app/src/main/java/nl/rijksmuseum/sample/data/repository/dataSource/RemoteCollectionDataSource.kt
+++
@@ -1,8 +0,0 @@
-package nl.rijksmuseum.sample.data.repository.dataSource
-
-import nl.rijksmuseum.sample.data.model.APIResponse
-import retrofit2.Response
-
-interface RemoteCollectionDataSource {
-    suspend fun getTopCollections():Response<APIResponse>
-}
